95c3a1127c558db7bcf4c1f90f4cf9dcb09b2265
Roman-Sanchez <romanfsanchez@gmail.com>
2015-01-31 19:50:20 -0700

    Removed world from output statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,8 @@ public class HelloWorldApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello GitHub World!");
+        System.out.println("Hello GitHub");
+        System.out.println("Testing...");
     }
     
 }
