bfd364749e3bb60e7e6883dbc1ee68402f0a0aff
Roman-Sanchez <romanfsanchez@gmail.com>
2015-01-31 19:47:11 -0700

    Added "Testing..." output

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorldApp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello GitHub World!");
+        System.out.println("Testing...");
     }
     
 }
